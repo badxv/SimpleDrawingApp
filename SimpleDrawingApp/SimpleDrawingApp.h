6fbedb9c96afb2253b5f36c46cef2300e784e215
badxv <194744955+badxv@users.noreply.github.com>
2025-06-22 20:49:57 -0700

    v0.1.1

--- SimpleDrawingApp/SimpleDrawingApp.h
+++ SimpleDrawingApp/SimpleDrawingApp.h
@@ -1,3 +1,16 @@
-#pragma once
-
+﻿#pragma once
+#include <windows.h>
+#include <gdiplus.h>
 #include "resource.h"
+using namespace Gdiplus;
+
+extern COLORREF penColor;
+extern int penWidth;
+extern POINT lastPoint;
+extern bool isDrawing;
+extern Bitmap* canvasBitmap;
+extern Graphics* canvasGraphics;
+extern ULONG_PTR gdiplusToken;
+
+void UpdatePenWidthDisplay();
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
